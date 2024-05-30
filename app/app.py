309109fa321bc9184c5ce5e707576c3cadfa3d26
tomershoham10 <tomershoham10@gmail.com>
2024-05-30 14:27:56 +0300

    feat: zipping file and resend it

--- app/app.py
+++ app/app.py
@@ -1,14 +1,64 @@
-from flask import Flask, request, jsonify
-import requests
+import logging
+from flask import Flask, request, send_file
+import pyminizip
+import os
+import io
 
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Create a Flask app
 app = Flask(__name__)
 
-NODEJS_BASE_URL = 'http://nodejs_microservice_url:port'
 
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    response = requests.get(f'{NODEJS_BASE_URL}/api/data')
-    return response.json(), response.status_code
+@app.route("/upload", methods=["POST"])
+def upload_file():
+    try:
+        if "file" not in request.files:
+            return "No file part", 400
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+        file = request.files["file"]
+
+        if file.filename == "":
+            return "No selected file", 400
+
+        logging.info(f"File: {file}")
+        password = "password"
+        # if not password:
+            # return "No password provided", 400
+
+        # Create a temporary file path for the uploaded file
+        input_file_path = os.path.join("/tmp", file.filename)
+        output_file_path = os.path.join("/tmp", f"{file.filename}.zip")
+
+        file.save(input_file_path)
+
+        # Compress the file with pyminizip and add a password
+        pyminizip.compress(input_file_path, None, output_file_path, password, 5)
+
+        # Read the compressed file into memory
+        with open(output_file_path, "rb") as f:
+            zip_data = f.read()
+
+        # Clean up the temporary files
+        os.remove(input_file_path)
+        os.remove(output_file_path)
+
+        logging.info("File zipped successfully with password")
+
+        # Send the zipped file back to the client
+        return send_file(
+            io.BytesIO(zip_data),
+            mimetype="application/zip",
+            as_attachment=True,
+            download_name=f"{file.filename}.zip",
+        )
+        # Save the uploaded file
+
+    except Exception as err:
+        logging.info(f"Error: {err}")
+
+
+if __name__ == "__main__":
+    # Run the Flask app on port 5000
+    app.run(host="0.0.0.0", port=5000)
--- dockerfile
+++ dockerfile
@@ -4,6 +4,15 @@ FROM python:3.9-slim
 # Set the working directory inside the container
 WORKDIR /app
 
+# Install build tools
+RUN apt-get update && apt-get install -y \
+    gcc \
+    make \
+    build-essential \
+    zlib1g-dev \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+
 # Copy the requirements file to the working directory
 COPY requirements.txt .
 
@@ -11,10 +20,10 @@ COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
 # Copy the Python service code to the working directory
-COPY . .
+COPY app /app
 
 # Expose port 5000 to allow external access
 EXPOSE 5000
 
 # Command to run the Python service
-CMD ["python", "app.py"]
+CMD ["gunicorn", "--bind", "0.0.0.0:5000", "app:app"]
