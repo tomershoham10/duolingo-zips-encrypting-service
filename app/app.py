06e23d5faa5d7af7580e4ae6aa09872118b403d2
tomershoham10 <tomershoham10@gmail.com>
2024-07-18 09:35:39 +0300

    feat: CI

--- app/app.py
+++ app/app.py
@@ -11,21 +11,21 @@ logging.basicConfig(level=logging.INFO)
 app = Flask(__name__)
 
 
-@app.route("/api/upload", methods=["POST"])
+@app.route("/api/encrypt-zip/upload", methods=["POST"])
 def upload_file():
     try:
         if "file" not in request.files:
             return "No file part", 400
 
         file = request.files["file"]
+        zip_password = request.form.get("zipPassword")
+        logging.info(f"zipPassword: {zip_password}")
 
         if file.filename == "":
             return "No selected file", 400
 
-        logging.info(f"File: {file}")
-        password = "password"
-        # if not password:
-            # return "No password provided", 400
+        if not zip_password:
+            zip_password = "password"
 
         # Create a temporary file path for the uploaded file
         input_file_path = os.path.join("/tmp", file.filename)
@@ -34,7 +34,7 @@ def upload_file():
         file.save(input_file_path)
 
         # Compress the file with pyminizip and add a password
-        pyminizip.compress(input_file_path, None, output_file_path, password, 5)
+        pyminizip.compress(input_file_path, None, output_file_path, zip_password, 5)
 
         # Read the compressed file into memory
         with open(output_file_path, "rb") as f:
